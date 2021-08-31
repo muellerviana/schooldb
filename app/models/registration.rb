48b8038f2ee531554c810b5607303213b953f705
Müller Viana <muellerviana@gmail.com>
2021-08-31 07:24:18 -0300

    Update registration.rb

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,5 +1,5 @@
 class Registration < ApplicationRecord
     has_and_belongs_to_many :students
-    has_and_belongs_to_many :assemblies
+    has_and_belongs_to_many :turmas #não sei mesmo como fazer essa intermediária
 
 end
